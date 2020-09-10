660b9430372d9d5c733f6e9306d04734cfca434d
Villecondectory17 <60339443+Villecondectory17@users.noreply.github.com>
2020-09-10 14:51:55 +0300

    Add files via upload
    1 Koodi

---
+++ Tkinter ikkuna.py
@@ -0,0 +1,23 @@
+# Avaa UI kirjasto 
+from tkinter import *
+
+# Tk() = uusi ikkuna. Sijoitetaan se muuttujaan root.
+root = Tk()
+
+# Funktio nappulan painamisen käsittelyyn 
+def IClicked(): 
+    #Kirjoita tekstit Labeliin, joka sijoitetaan ikkunaan, nimeltä root.
+    buttonClickLabel=Label(root, text="Nappulaa painettu")
+    buttonClick.pack()
+
+
+# Esimerkki disabled nappulasta. 
+disabledButton = Button(root, text="Paina nappulaa", state=DISABLED)
+disabledButton.pack() 
+
+# Esimerkki aktiivisesta nappulasta.
+activeButton = Button(root, text="Paina nappulaa", padx=50, pady=50, command=IClicked, fg="red")
+activeButton.pack()
+
+# Käynnistä root niminen ikkuna. 
+root.mainloop() 
