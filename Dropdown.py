92c95c39d9a6f843ca7ce394973f50c3d8d85643
Villecondectory17 <60339443+Villecondectory17@users.noreply.github.com>
2020-09-14 12:35:26 +0300

    Update Dropdown.py
    Pieni muutos

--- Dropdown.py
+++ Dropdown.py
@@ -1,1 +1,30 @@
-mm__
+# Dropdown esimerkki 
+
+from tkinter import *
+
+root = Tk()
+root.title("Alasvetovalikko")
+root.geometry("400x400") 
+
+options=[
+    "ensimmäinen",
+    "Toinen",
+    "Kolmas",
+    "Neljäs",
+    "Viides"
+]
+
+selectItem = StringVar()
+selectItem.set(options[0])
+
+def show():
+    myLabel=Label(root, text=selectItem.get()).pack()
+    
+drop = Optionmenu(root, selectItem, *options
+drop.pack()
+                  
+myButton = Button(root, text="Valitse tästä", command=show).pack()
+
+root.mainloop()
+  
+  
