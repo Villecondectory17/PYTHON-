c526fe46e99b48262b931a50ae8ef559726a01fb
Villecondectory17 <60339443+Villecondectory17@users.noreply.github.com>
2020-09-14 14:57:45 +0300

    Update Messagebox.py
    muutos2

--- Messagebox.py
+++ Messagebox.py
@@ -6,7 +6,10 @@ root = TK()
 root.title("Messagebox-harjoitus")
 
 # Testaile messageboxin vaihtoehtoja: showinfo, showwarning, askquestion, asokcancel, asyesn
-def popup(): 
+def popup(msg): 
+    root = tkinter.Tk() 
+    root.widthdraw() 
+    popup = tk.Tk() 
     response = messagebox.askyesno("Messagebox haluaa sanoa: ", "Täällä ollaan")
     # Jos käyttäjä painaa Yes:
     if response == 1:
@@ -14,7 +17,13 @@ def popup():
     # Jos käyttäjä painaa No:
     else: 
         Label(root, text="Painoit No-nappia!").pack()
-
+        
+        messagebox.showinfo("Info") 
+        messagebox.showwarning("Warning")
+        messagebox.askquestion("question")
+        messagebox.askokcancel("okcancel")
+        messagebox.asyesn("")
+    
 # Nappula, joka kutsuu funktiota popup 
 Button(root, text="Viesti", command=popup).pack()
 root.mainloop() 
