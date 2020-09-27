32ee7ba38d74ae91e07ce7395185f0798a3988b8
Villecondectory17 <60339443+Villecondectory17@users.noreply.github.com>
2020-09-27 20:01:48 +0300

    Update Names.py
    Kommentit koodiin

--- Names.py
+++ Names.py
@@ -1,13 +1,17 @@
-import tkinter as tk
+import tkinter as tk 
 
+# Määritellään syötekenttien otsikot
 fields = 'Last Name', 'First Name', 'Job', 'Country'
 
+# Luetaan syötekenttien otsikot ja niihin syötetyt arvot ja tulostetaan ne konsolille 
 def fetch(entries): 
     for entry in entries:
         field = entry[0]
         text = entry[1].get()
         print('%s: "%s"' % (field, text))
 
+# Luodaan parilista jossa ovat syötekenttien otsikot yhdistettynä vastaavaan syötekenttään. 
+# Sijoittelee elementit käyttöliittymään 
 def makeform(root, fields):
     entries = []
     for field in fields: 
@@ -19,10 +23,14 @@ def makeform(root, fields):
         ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
         entries.append((field, ent))
     return entries
-
+ 
 if __name__ == '__main__':
+
+    # Ikkunan ja kenttien alustukset 
     root = tk.Tk()
     ents = makeform(root, fields)
+
+    # Painikkeiden toiminnot 
     root.bind('<Return>', (lambda event, e=ents: fetch(e)))
     b1 = tk.Button(root, text='Show',
                     command=(lambda e=ents: fetch(e))
