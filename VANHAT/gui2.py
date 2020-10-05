5d118bef2976d2da328e4a92cc0c6abea2150289
Villecondectory17 <60339443+Villecondectory17@users.noreply.github.com>
2020-10-05 14:38:09 +0300

    Update gui2.py
    Scriptin lähetys

--- VANHAT/gui2.py
+++ VANHAT/gui2.py
@@ -1,121 +1,144 @@
 from tkinter import *
-from tkinter import ttk 
-from csv import DictWriter 
-import tkinter 
 import os 
-import tkinter as tk
-win = tk.Tk() 
-win = tkinter.Tk()
-win.title('GUI')
 
-# create labels
-# widgets -- > label, buttons, radio buttons - tk , ttk 
-name_label = ttk.Label(win, text='Enter your name : ')
-name_label.grid(row=0, column=0, sticky=tk.W)   
+def delete2():
+    screen3.destroy()
 
-email_label = ttk.Label(win, text='Enter your email : ')
-email_label.grid(row=1, column=0, sticky=tk.W) 
+def delete3():
+    screen4.destroy()   
 
-age_label = ttk.Label(win, text = 'Enter your age : ') 
-age_label.grid(row=2, column=0, sticky=tk.W)
+def delete4():
+    screen5.destroy()   
 
-# create entry box
-name_var = tk.StringVar() 
-name_entrybox = ttk.Entry(win, width=16, textvariable = name_var) 
-name_entrybox.grid(row=0, column=1) 
-name_entrybox.focus()
+def login_sucess():
+    global screen3 
+    screen3 = Toplevel(screen)
+    screen3.title("Success") 
+    screen3.geometry("150x100")
+    Label(screen3, text = "Login Sucess").pack()
+    Button(screen3, text = "OK", command =delete2).pack()    
 
-email_var = tk.StringVar() 
-email_entrybox = ttk.Entry(win, width=16, textvariable = email_var) 
-email_entrybox.grid(row=1, column=1) 
+def password_not_recognised():
+   global screen4
+   screen4 = Toplevel(screen)
+   screen4.title("Success") 
+   screen4.geometry("150x100")
+   Label(screen4, text = "Password Error").pack() 
+   Button(screen4, text = "OK", command =delete3).pack()    
 
-age_var = tk.StringVar() 
-age_entrybox = ttk.Entry(win, width=16, textvariable = age_var) 
-age_entrybox.grid(row=2, column=1) 
+def user_not_found():
+   global screen5
+   screen5 = Toplevel(screen)
+   screen5.title("Success") 
+   screen5.geometry("150x100")
+   Label(screen5, text = "User Not Found").pack() 
+   Button(screen5, text = "OK", command =delete4).pack()   
 
-# create combobox 
-gender_var = tk.StringVar() 
-gender_combobox = ttk.Combobox(win, width=14, textvariable=gender_var, state='readonly') 
-gender_combobox['values'] = ('Male', 'Female', 'Other') 
-gender_combobox.current(0)
-gender_combobox.grid(row=3, column=0) 
+def register_user():
+   print("working") 
 
-# radio button 
-# student , teacher 
-usertype = tk.StringVar() 
-radiobtn1 = ttk.Radiobutton(win, text='Student', value='Student', variable=usertype)
-radiobtn1.grid(row=4, column=0)   
+   username_info = username.get()
+   password_info = password.get()
 
-radiobtn2 = ttk.Radiobutton(win, text='Teacher', value='Teacher', variable=usertype)
-radiobtn2.grid(row=4, column=1) 
+    file=open(username_info, "w") 
+    file.write(username_info+"\n")
+    file.write(password_info) 
+    file.close() 
 
-# check button 
-checkbtn_var = tk.IntVar()  
-checkbtn = ttk.Checkbutton(win, text='check if you want to subscribe to our newsletter', variable=checkbtn_var) 
-checkbtn.grid(row=5,columnspan=3) 
-# checkbtn.select()
-# create button
+    username_entry.delete(0, END)
+    password_entry.delete(0, END) 
 
-def action():
-    username = name_var.get()
-    userage = age_var.get()
-    user_email = email_var.get()
-    print(f'{username} is {userage} years old ,  {user_email}')
-    user_gender = gender_var.get()
-    user_type = usertype.get()
-    if checkbtn_var.get() == 0:
-        subscriped = 'NO'
+    Label(screen1,  text = "Registration Sucess", fg = "green", font = ("calibri", 11)).pack() 
+
+def login_verify():
+
+   username1 = username_verify.get()
+   password1 = password_verify.get() 
+   username_entry1.delete(0, END) 
+   password_entry1.delete(0, END)  
+
+   list_of_files = os.listdir() 
+   if username1 in list_of_files: 
+     file1 = open(username1, "r") 
+     verify = file1.read().splitlines() 
+     if password1 in verify:
+          login_sucess() 
+     else:
+        password_not_recognised()
+
     else:
-        subscribed = 'Yes'
-    print(user_gender, user_type, subscribed)
+        user_not_found() 
 
-    with open('file.txt', 'a') as f:
-        f.write(f'{username},{userage},{user_email},{user_gender},{user_type},{subscribed}\n') 
-
-    name_entrybox.delete(0, tk.END)
-    age_entrybox.delete(0, tk.END) 
-    email_entrybox.delete(0, tk.END) 
-    name_label.configure(foreground='#b388ff') 
-
-    # submit_button.configure(foreground='Blue')
-
-# write to csv files 
-def action():
-    username = name_var.get()
-    userage = age_var.get()
-    user_email = email_var.get()
-    user_type = usertype.get()
-    user_gender = gender_var.get() 
-    if checkbtn_var.get() == 0:
-        subscriped = 'NO'
-    else:
-        subscribed = 'Yes'
-    
-    # write to csv file 
-    with open('file.csv', 'a', newline='') as f: 
-        dict_writer = DictWriter(f, fieldnames=['UserName', 'User Email Address', 'User Age', 'User Gender', 'User Type', 'Subcribed'])  
-        if os.stat('file.csv').st_size==0: 
-            dict_writer.writeheader() 
-        
-        dict_writer.writerow({
-            'UserName' : username,
-            'User Email Address' : user_email,
-            'User Age' : userage,
-            'User Gender' : user_gender, 
-            'User Type' : user_type, 
-            'Subcribed' : subscriped
-        }) 
-    
-    name_entrybox.delete(0, tk.END)
-    age_entrybox.delete(0, tk.END) 
-    email_entrybox.delete(0, tk.END) 
-    name_label.configure(foreground='#b388ff') 
- 
-
-submit_button = tk.Button(win, text='Submit', command=action) 
-submit_button.grid(row=6, column=0) 
-
-win.mainloop() 
+#print("User not found !")  
+#print("working...") 
+   
 
 
+def register():
+   global screen1 
+   screen1 = Toplevel(screen) 
+   screen1.title("Register")
+   screen1.geometry("300x250")
 
+   global username
+   global password 
+   global username_entry
+   global password_entry
+   username = StringVar()
+   password = StringVar() 
+
+   Label(screen1, text = "Please enter details below").pack() 
+   Label(screen1, text = "").pack() 
+   Label(screen1, text = "Username * ").pack()
+   
+   username_entry = Entry(screen1, textvariable = username)
+   username_entry.pack() 
+   Label(screen1, text = "Password * ").pack()
+   password_entry = Entry(screen1, textvariable = password)
+   password_entry.pack() 
+   Label(screen1, text = "").pack() 
+   Button(screen1, text = "Register", width = 10, height = 1, command = register_user).pack() 
+
+def login():
+   global screen2
+   screen2 = Toplevel(screen)
+   screen2.title("Login")
+   screen2.geometry("300x250")
+   Label(screen2, text = "Please enter details below to login").pack() 
+   Label(screen2, text = "").pack() 
+
+   global username_verify
+   global password_verify 
+
+   username_verify = StringVar()
+   password_verify = StringVar() 
+
+   global username_entry1
+   global password_entry1 
+
+   Label(screen2, text = "Username * ").pack()
+   username_entry1 = Entry(screen2, textvariable = username_verify) 
+   username_entry1.pack()
+   Label(screen2, text = "").pack() 
+   Label(screen2, text = "Password * ").pack()
+   password_entry1 = Entry(screen2, textvariable = password_verify) 
+   password_entry1.pack() 
+   Label(screen2, text = "").pack() 
+   Button(screen2, text = "Login", width = 10, height = 1, command = login_verify).pack() 
+   #print("Login session started") 
+
+
+def main_screen():
+    global screen 
+    screen = Tk()
+    screen.geometry("300x250")
+    screen.title("Notes 1.0")
+    Label(text = "Notes 1.0", bg = "grey", width = "300", height = "2", font = ("Calibri", 13)).pack() 
+    Label(text = "").pack() 
+    Button(text = "Login", height = "2", width = "30", command = login).pack() 
+    Label(text = "").pack() 
+    Button(text= "Register", height = "2", width = "30", command = register).pack()  
+
+    screen.mainloop() 
+
+main_screen() 
