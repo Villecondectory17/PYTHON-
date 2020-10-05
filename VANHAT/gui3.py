45bb9062241bce120beb3db09935d6afb3ccba29
Villecondectory17 <60339443+Villecondectory17@users.noreply.github.com>
2020-10-05 11:42:19 +0300

    Update gui3.py
    Scriptin lähetys

--- VANHAT/gui3.py
+++ VANHAT/gui3.py
@@ -1,121 +1,127 @@
 from tkinter import *
-from tkinter import ttk 
-from csv import DictWriter 
-import tkinter 
 import os 
-import tkinter as tk
-win = tk.Tk() 
-win = tkinter.Tk()
-win.title('GUI')
 
-# create labels
-# widgets -- > label, buttons, radio buttons - tk , ttk 
-name_label = ttk.Label(win, text='Enter your name : ')
-name_label.grid(row=0, column=0, sticky=tk.W)   
+def delete2():
+    screen3.destroy()
 
-email_label = ttk.Label(win, text='Enter your email : ')
-email_label.grid(row=1, column=0, sticky=tk.W) 
+def delete3():
+    screen4.destroy()
 
-age_label = ttk.Label(win, text = 'Enter your age : ') 
-age_label.grid(row=2, column=0, sticky=tk.W)
+def delete4():
+    screen5.destroy()
 
-# create entry box
-name_var = tk.StringVar() 
-name_entrybox = ttk.Entry(win, width=16, textvariable = name_var) 
-name_entrybox.grid(row=0, column=1) 
-name_entrybox.focus()
+def logout():
+    screen7.destroy()
 
-email_var = tk.StringVar() 
-email_entrybox = ttk.Entry(win, width=16, textvariable = email_var) 
-email_entrybox.grid(row=1, column=1) 
+def saved():
+    screen10 = Toplevel(screen)
+    screen10.title("Saved") 
+    screen10.geometry("100x100") 
+    Label(screen10, text = "Saved").pack() 
 
-age_var = tk.StringVar() 
-age_entrybox = ttk.Entry(win, width=16, textvariable = age_var) 
-age_entrybox.grid(row=2, column=1) 
+def save():
 
-# create combobox 
-gender_var = tk.StringVar() 
-gender_combobox = ttk.Combobox(win, width=14, textvariable=gender_var, state='readonly') 
-gender_combobox['values'] = ('Male', 'Female', 'Other') 
-gender_combobox.current(0)
-gender_combobox.grid(row=3, column=0) 
+    filename = raw_filename.get()
+    notes = raw_notes.get() 
 
-# radio button 
-# student , teacher 
-usertype = tk.StringVar() 
-radiobtn1 = ttk.Radiobutton(win, text='Student', value='Student', variable=usertype)
-radiobtn1.grid(row=4, column=0)   
+    data = open(filename, "w")
+    data.write(notes)
+    data.close()
 
-radiobtn2 = ttk.Radiobutton(win, text='Teacher', value='Teacher', variable=usertype)
-radiobtn2.grid(row=4, column=1) 
+    saved() 
 
-# check button 
-checkbtn_var = tk.IntVar()  
-checkbtn = ttk.Checkbutton(win, text='check if you want to subscribe to our newsletter', variable=checkbtn_var) 
-checkbtn.grid(row=5,columnspan=3) 
-# checkbtn.select()
-# create button
+    #print("saved") 
 
-def action():
-    username = name_var.get()
-    userage = age_var.get()
-    user_email = email_var.get()
-    print(f'{username} is {userage} years old ,  {user_email}')
-    user_gender = gender_var.get()
-    user_type = usertype.get()
-    if checkbtn_var.get() == 0:
-        subscriped = 'NO'
-    else:
-        subscribed = 'Yes'
-    print(user_gender, user_type, subscribed)
+def create_notes():
+    global raw_filename 
+    raw_filename = StringVar() 
+    global raw_notes 
+    raw_notes = StringVar()
 
-    with open('file.txt', 'a') as f:
-        f.write(f'{username},{userage},{user_email},{user_gender},{user_type},{subscribed}\n') 
+    screen9 = Toplevel(screen)
+    screen9.title("Info") 
+    screen9.geometry("300x250") 
+    Label(screen9, text = "Please enter a filename for the note below : ").pack()
+    Entry(screen9, textvariable = raw_filename).pack() 
+    Label(screen9, text = "Please enter the notes for the file below : ").pack() 
+    Entry(screen9, textvariable = raw_notes).pack() 
+    Button(screen9, text = "Save", command = save).pack() 
 
-    name_entrybox.delete(0, tk.END)
-    age_entrybox.delete(0, tk.END) 
-    email_entrybox.delete(0, tk.END) 
-    name_label.configure(foreground='#b388ff') 
-
-    # submit_button.configure(foreground='Blue')
-
-# write to csv files 
-def action():
-    username = name_var.get()
-    userage = age_var.get()
-    user_email = email_var.get()
-    user_type = usertype.get()
-    user_gender = gender_var.get() 
-    if checkbtn_var.get() == 0:
-        subscriped = 'NO'
-    else:
-        subscribed = 'Yes'
-    
-    # write to csv file 
-    with open('file.csv', 'a', newline='') as f: 
-        dict_writer = DictWriter(f, fieldnames=['UserName', 'User Email Address', 'User Age', 'User Gender', 'User Type', 'Subcribed'])  
-        if os.stat('file.csv').st_size==0: 
-            dict_writer.writeheader() 
-        
-        dict_writer.writerow({
-            'UserName' : username,
-            'User Email Address' : user_email,
-            'User Age' : userage,
-            'User Gender' : user_gender, 
-            'User Type' : user_type, 
-            'Subcribed' : subscriped
-        }) 
-    
-    name_entrybox.delete(0, tk.END)
-    age_entrybox.delete(0, tk.END) 
-    email_entrybox.delete(0, tk.END) 
-    name_label.configure(foreground='#b388ff') 
- 
-
-submit_button = tk.Button(win, text='Submit', command=action) 
-submit_button.grid(row=6, column=0) 
-
-win.mainloop() 
+def view_notes1(): 
+    filename1 = raw_filename1.get()
+    data = open(filename1, "r") 
+    data1 = data.read() 
+    screen12 = Toplevel(screen) 
+    screen12.title("Notes") 
+    screen12.geometry("400x400")
+    Label(screen12, text = data1).pack() 
+    #Label(screen12, text = all_files).pack() 
+    #data.close() 
 
 
+def view_notes():
+    screen11 = Toplevel(screen) 
+    screen11.title("Info") 
+    screen11.geometry("250x250") 
+    all_files = os.listdir()
+    Label(screen11, text = "Please use one of the filenames below").pack() 
+    Label(screen11, text = all_files).pack()
+    global raw_filename1 
+    raw_filename1 = StringVar() 
+    Entry(screen11, textvariable=raw_filename1).pack() 
+    Button(screen11, command=view_notes1, text = "OK").pack()
 
+    #Label(screen9, text = "Welcome to the dashboard").pack()
+
+
+def session():
+    screen8 = Toplevel(screen)
+    screen8.title("dashboard")
+    screen8.geometry("400x400")
+    Label(screen8, text = "Welcome to the dashboard").pack()
+    Button(screen8, text = "Create Note", command = create_notes).pack() 
+    Button(screen8, text = "View Note", command = view_notes).pack() 
+    Button(screen8, text = "Delete Note").pack() 
+
+def login_sucess():
+    session()
+
+def password_not_recognised():
+    global screen4 
+    screen4.title("Success")
+    screen4.geometry("150x100")
+    Label(screen4, text = "Password Error").pack()
+    Button(screen4, text = "OK", command =delete3).pack()
+
+def user_not_found():
+    global screen5 
+    screen5 = Toplevel(screen)
+    screen5.title("Success")
+    screen5.geometry("150x100")
+    Label(screen5, text = "User Not Found").pack()
+    Button(screen5, text = "OK", command =delete4).pack()
+
+def register_user():
+    print("Working")
+    global username_info
+    username_info = username.get()
+    password_info = password.get()
+
+    file=open(username_info, "w")
+    file.write(username_info+"\n")
+    file.write(password_info)
+    file.close() 
+
+    username_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+    Label(screen1, text = "Registration Sucess", fg = "green" ,font = ("calibri", 11)).pack()
+
+def login_verify():
+    global username1
+    username1 = username_verify.get()
+    password1 = password_verify.get()
+    username_entry1.delete(0, END)
+    password_entry1.delete(0, END)
+
+    list_of_files 
